511a24281721294b3e2f45dfbf7b2f83c8f275cf
Fitsum <fitbez@gmail.com>
2022-12-12 13:23:40 -0500

    week-2

---
+++ week-2/02-React/01-Activities/02-Stu_HelloReact/Unsolved/src/components/HelloReact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloReact = () => {
+  return (
+    <div>
+      <p>Hello World!</p>
+      <div>Hello React</div>
+    </div>
+  );
+};
+
+export default HelloReact;
